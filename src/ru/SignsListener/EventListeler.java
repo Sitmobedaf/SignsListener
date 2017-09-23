72911155bdcb3ba4bfd962399070d54083ab253d
Kenig <Sitmobedaf@mail.ru>
2017-09-23 19:17:18 +0300

    Update to 1.3
     - Add new options
     - Fix some errors

--- src/ru/SignsListener/EventListeler.java
+++ src/ru/SignsListener/EventListeler.java
@@ -29,19 +29,34 @@ public class EventListeler implements Listener {
 
 	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
 	public void onSignChange(SignChangeEvent e) throws FileNotFoundException {
-		if (e.getPlayer() != null) {
+		if (!e.isCancelled() && e.getPlayer() != null) {
 			Player p = e.getPlayer();
 			if (!e.getLine(0).equals("") || !e.getLine(1).equals("") || !e.getLine(2).equals("") || !e.getLine(3).equals("")) {
 				if (conf.getBoolean("SignsListener.Enabled")) {
-					String l0 = e.getLine(0);
-					String l1 = e.getLine(1);
-					String l2 = e.getLine(2);
-					String l3 = e.getLine(3);
+					String line1 = e.getLine(0);
+					String line2 = e.getLine(1);
+					String line3 = e.getLine(2);
+					String line4 = e.getLine(3);
+					StringBuilder builder = new StringBuilder();
+					builder.append(line1).append(conf.getString("SignsListener.Separator")).append(line2).append(conf.getString("SignsListener.Separator")).append(line3).append(conf.getString("SignsListener.Separator")).append(line4);
+					String text = builder.toString();
 					String x = "" + p.getLocation().getBlockX();
 					String y = "" + p.getLocation().getBlockY();
 					String z = "" + p.getLocation().getBlockZ();
 					String w = p.getWorld().getName();
-					String signsFormat = conf.getString("SignsListener.OutputFormat").replace("<player>", p.getName()).replace("<world>", w).replace("<x>", x).replaceAll("<y>", y).replaceAll("<z>", z).replaceAll("<line1>", l0).replaceAll("<line2>", l1).replaceAll("<line3>", l2).replaceAll("<line4>", l3);
+					if (!p.hasPermission("SignsListener.useFormatting") && conf.getBoolean("SignsListener.BlockFormatting")) {
+						if (text.contains("§")) {
+							p.sendMessage(conf.getString("SignsListener.FormattingBlocked").replaceAll("&", "§"));
+							e.setCancelled(true);
+						}
+					}
+					String signsFormat = null;
+					try {
+						signsFormat = conf.getString("SignsListener.OutputFormat").replace("<player>", p.getName()).replace("<world>", w).replace("<x>", x).replaceAll("<y>", y).replaceAll("<z>", z).replaceAll("<text>", Matcher.quoteReplacement(text));
+					} catch (Exception ex) {
+						p.sendMessage(conf.getString("SignsListener.IllegalSymbols").replaceAll("&", "§"));
+						e.setCancelled(true);
+					}
 					if (conf.getBoolean("SignsListener.UseDifferentFile")) {
 						try (FileWriter signLog = new FileWriter(path + "signs.txt", true); BufferedWriter signBuff = new BufferedWriter(signLog); PrintWriter outSign = new PrintWriter(signBuff)) {
 							outSign.println(signsFormat);
@@ -68,23 +83,26 @@ public class EventListeler implements Listener {
 						title = e.getNewBookMeta().getTitle();
 					}
 					List<String> pages = e.getNewBookMeta().getPages();
-					String text = pages.toString().replaceAll("\n", " + ");
+					String text = null;
+					for (String onePage : pages) {
+						StringBuilder builder = new StringBuilder();
+						builder.append(onePage);
+						text = builder.toString().replaceAll("\n", conf.getString("BooksListener.Separator")).replaceAll("§0", "");
+					}
 					String x = "" + p.getLocation().getBlockX();
 					String y = "" + p.getLocation().getBlockY();
 					String z = "" + p.getLocation().getBlockZ();
 					String w = p.getWorld().getName();
 					if (!p.hasPermission("BooksListener.useFormatting") && conf.getBoolean("BooksListener.BlockFormatting")) {
-						for (String text1 : e.getNewBookMeta().getPages()) {
-							if (text1.contains("§")) {
-								e.setCancelled(true);
-								p.sendMessage(conf.getString("BooksListener.FormattingBlocked").replaceAll("&", "§"));
-							}
+						if (text.contains("§")) {
+							p.sendMessage(conf.getString("BooksListener.FormattingBlocked").replaceAll("&", "§"));
+							e.setCancelled(true);
 						}
 					}
 					String booksFormat = null;
 					try {
-						booksFormat = conf.getString("BooksListener.OutputFormat").replace("<player>", p.getName()).replace("<world>", w).replace("<x>", x).replaceAll("<y>", y).replaceAll("<z>", z).replaceAll("<title>", title).replaceAll("<text>", Matcher.quoteReplacement(text.toString()));
-					} catch (IllegalArgumentException ex) {
+						booksFormat = conf.getString("BooksListener.OutputFormat").replace("<player>", p.getName()).replace("<world>", w).replace("<x>", x).replaceAll("<y>", y).replaceAll("<z>", z).replaceAll("<title>", title).replaceAll("<text>", Matcher.quoteReplacement(text));
+					} catch (Exception ex) {
 						p.sendMessage(conf.getString("BooksListener.IllegalSymbols").replaceAll("&", "§"));
 						e.setCancelled(true);
 					}
